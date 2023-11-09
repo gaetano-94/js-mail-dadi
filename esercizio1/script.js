9a9efd77c6bc265f89ed348f789d8f2e29054767
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-09 15:58:57 +0100

    feat: prompt piu array per email

--- esercizio1/script.js
+++ esercizio1/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+/*
+1. Mail
+   Chiedi all’utente la sua email,
+   controlla che sia nella lista di chi può accedere,
+   stampa un messaggio appropriato sull’esito del controllo.
+
+1) creare una variabile prompt, dove l'utente potrà inserire la sua email
+
+2) creare un ARRAY con delle ipotetiche email, cosi da poter fare un ciclo e controllare se l'email del utente e presente
+
+3) creare un ciclo con FOR collegato a un variabile ARRAY, facendo ciò possiamo vedere se l'email ci sia nella lista oppure no
+*/
+
+//1)
+const emailUtente = prompt('inserisci la tua email');
+console.log(emailUtente);
+
+//2)
+const varArray = ['gaetano@gmail.com', 'luca@libero.it', 'paolo@hotmail.it'];
+console.log(varArray);
+
+//3)
